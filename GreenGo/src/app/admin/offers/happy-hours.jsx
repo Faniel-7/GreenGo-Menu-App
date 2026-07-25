d618a82d6a2843ad724452d8e5ef34c7c314db65
Faniel Negasi <ugr18809916@office.mu.edu.et>
2026-07-25 19:50:42 -0400

    Add Happy Hours component to display happy hour offers

---
+++ GreenGo/src/app/admin/offers/happy-hours.jsx
@@ -0,0 +1,11 @@
+import OfferList from "../../../components/admin/OfferList";
+
+export default function HappyHours() {
+  return (
+    <OfferList
+      type="happy_hour"
+      title="Happy Hours"
+      icon="🔥"
+    />
+  );
+}
