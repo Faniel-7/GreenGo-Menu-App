a8e98ed470beaee7fbcf2dae525347117c8494a8
Faniel Negasi <ugr18809916@office.mu.edu.et>
2026-07-25 19:51:02 -0400

    Add Promotions component to display promotional offers

---
+++ GreenGo/src/app/admin/offers/promotion.jsx
@@ -0,0 +1,11 @@
+import OfferList from "../../../components/admin/OfferList";
+
+export default function Promotions() {
+  return (
+    <OfferList
+      type="promotion"
+      title="Promotions"
+      icon="🎉"
+    />
+  );
+}
