be42e1055beb9163a1457b72a111e18a60ed066d
Faniel Negasi <ugr18809916@office.mu.edu.et>
2026-07-25 19:50:23 -0400

    Add Discounts component to display discount offers

---
+++ GreenGo/src/app/admin/offers/discounts.jsx
@@ -0,0 +1,11 @@
+import OfferList from "../../../components/admin/OfferList";
+
+export default function Discounts() {
+  return (
+    <OfferList
+      type="discount"
+      title="Discounts"
+      icon="🏷️"
+    />
+  );
+}
