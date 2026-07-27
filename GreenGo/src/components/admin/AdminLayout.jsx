0b25a7d1111755e153aaa4fa063252e78f5376e6
Faniel Negasi <ugr18809916@office.mu.edu.et>
2026-07-26 20:38:04 -0400

    Add logout functionality to AdminLayout component

--- GreenGo/src/components/admin/AdminLayout.jsx
+++ GreenGo/src/components/admin/AdminLayout.jsx
@@ -1,8 +1,10 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Text, Alert } from "react-native";
 import useResponsive from "../../hooks/useResponsive";
-
 import AdminSidebar from "./AdminSidebar";
 import AdminBottomTabs from "./AdminBottomTabs";
+import { supabase } from "../../lib/supabase";
+import { router } from "expo-router";
+
 
 
 export default function AdminLayout({children}) {
@@ -13,6 +15,24 @@ export default function AdminLayout({children}) {
     isDesktop
   } = useResponsive();
 
+  async function logout(){
+
+const { error } = await supabase.auth.signOut();
+
+if(error){
+
+Alert.alert(
+"Error",
+error.message
+);
+
+return;
+
+}
+
+router.replace("../../admin/login");
+
+}
 
   return (
 
@@ -26,9 +46,24 @@ export default function AdminLayout({children}) {
 
       <View style={styles.content}>
 
-        {children}
+  <View style={styles.topBar}>
 
-      </View>
+    <TouchableOpacity
+      style={styles.logoutButton}
+      onPress={logout}
+    >
+
+      <Text style={styles.logoutText}>
+        Logout
+      </Text>
+
+    </TouchableOpacity>
+
+  </View>
+
+  {children}
+
+</View>
 
 
       {isMobile && (
@@ -53,6 +88,25 @@ const styles = StyleSheet.create({
 
   content:{
     flex:1,
-  }
+  },
+  topBar:{
+height:60,
+justifyContent:"center",
+alignItems:"flex-end",
+paddingHorizontal:20,
+},
+
+logoutButton:{
+backgroundColor:"#f4b400",
+paddingVertical:8,
+paddingHorizontal:20,
+borderRadius:10,
+},
+
+logoutText:{
+color:"#000",
+fontWeight:"900",
+fontSize:15,
+},
 
 });
